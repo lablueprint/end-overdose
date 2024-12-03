cd67d8dc52ef680a4475872ad1ea9ce905b5da53
youssef <mehelbay@gmail.com>
2024-12-02 22:32:31 -0800

    Add interactive quiz with correctness checking and percentage score

--- src/app/quiz/components/Question.tsx
+++ src/app/quiz/components/Question.tsx
@@ -4,18 +4,24 @@ import { useState } from 'react';
 interface QuestionProps {
     question: string,
     answers: string[],
+    onAnswerSelected: (answerIndex: number) => void; // Add callback for answer selection
 }
 
-export default function Question({ question, answers }: QuestionProps)  {
+export default function Question({ question, answers, onAnswerSelected }: QuestionProps)  {
     
 
     return (
         <>
             <div>{question}</div>
-            <div>{answers[0]}</div>
-            <div>{answers[1]}</div>
-            <div>{answers[2]}</div>
-            <div>{answers[3]}</div>
+            {answers.map((answer, index) => (
+                <button 
+                    key={index} 
+                    onClick={() => onAnswerSelected(index)}
+                    style={{ display: 'block', margin: '10px 0' }}
+                >
+                    {answer}
+                </button>
+            ))}
         </>
 
     )
