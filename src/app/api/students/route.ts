ea433886be1c6f4b0dc2ed11fd3e773c6d7f3cd2
cherylwu834 <124432604+cherylwu834@users.noreply.github.com>
2024-12-02 21:52:14 -0800

    Added student POST API
    Co-authored-by: Sanjay Krishna <sanjay.krishnas@gmail.com>

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -3,3 +3,41 @@ import firebase_app from '@/firebase/config';
 import { getFirestore, addDoc, collection } from 'firebase/firestore';
 
 const db = getFirestore(firebase_app);
+
+export async function POST(request: NextRequest) {
+    console.log('HERE');
+    try {
+        const requestData = await request.json();
+        console.log('Request Data:', requestData); // Log request data for debugging
+
+        const newStudentRef = await addDoc(collection(db, 'admins'), {
+            student_id: requestData.student_id,
+            name: {
+                first: requestData.name.first,
+                last: requestData.name.last,
+            },
+            school_name: requestData.school_name,
+            badges: [],
+            course_completion: {},
+        });
+
+        console.log('Document written with ID:', newStudentRef.id);
+
+        // Return success response
+        return NextResponse.json({
+            message: 'Admin user created successfully',
+            userId: newStudentRef.id,
+        });
+    } catch (error) {
+        console.error('Error creating document:', error);
+
+        // Ensure a proper response is returned in case of an error
+        return NextResponse.json(
+            {
+                message: 'Failed to create admin user',
+                error: error.message || 'Unknown error',
+            },
+            { status: 500 }
+        );
+    }
+}
