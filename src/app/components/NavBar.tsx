690f68471daa3591ac5efd8b45a7b27416900a0b
Joyce Chen <joyce.chen721@gmail.com>
2024-12-19 10:41:43 +0800

    create a navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,45 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className="bg-black p-4 shadow-md">
+            <ul className="flex justify-start space-x-6">
+                <li className="relative">
+                    <Link href="/">
+                        <Image
+                            src="/logo.png"
+                            alt="logo"
+                            width={100}
+                            height={50}
+                        />
+                    </Link>
+                </li>
+
+                <li className="relative text-white hover:text-gray-200">
+                    <Link href={'/courses'}>Courses</Link>
+                </li>
+
+                <li className="relative text-white hover:text-gray-200">
+                    <Link href={'/profile'}>Profile</Link>
+                </li>
+
+                <li className="relative text-white hover:text-gray-200">
+                    <Link href={'/admin'}>Admin</Link>
+                </li>
+
+                <li className="relative text-white hover:text-gray-200">
+                    <Link href={'/login'}>Login</Link>
+                </li>
+
+                <li className="relative text-white hover:text-gray-200">
+                    <Link href={'/signup'}>Signup</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
