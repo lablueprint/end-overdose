a071d173f1edcf31ee30f93c578633650a97de04
cheyennelu17 <cheyennelu17@g.ucla.edu>
2025-01-14 01:04:22 -0500

    courses3, course component

--- src/app/courses/components/Course.tsx
+++ src/app/courses/components/Course.tsx
@@ -18,11 +18,15 @@ export default function Course({
     };
     return (
         <>
-            <div style={{ cursor: 'pointer' }} onClick={handleClick}>
-                Title: {courseTitle}, Progress:{courseProgress}
-            </div>
-            <div>
-                <progress value={courseProgress} max={100}></progress>
+            <div className="relative cursor-pointer border-2 w-80 min-h-64 m-5 p-5 rounded-xl">
+                <div onClick={handleClick}>
+                    <h2 className="leading-8 font-bold">{courseTitle}</h2>
+                </div>
+                <div>
+                    <progress value={courseProgress} max={100}></progress>{' '}
+                    {courseProgress}
+                </div>
+                <div className="absolute w-32 border-2 h-28 bottom-5 right-5"></div>
             </div>
         </>
     );
