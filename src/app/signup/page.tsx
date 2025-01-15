ebe14b42a36912ccfa54fc6472d0065e6b6be2aa
Fiona Peng <fionapeng1616@gmail.com>
2025-01-14 23:32:42 -0800

    fix result.userId

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,15 +5,21 @@ const SignupPage = () => {
     return (
         <div>
             <h1>Signup</h1>
-            <Link href="/signup/school-admin">
-                <button>School Admin Signup</button>
-            </Link>
-            <Link href="/login/students">
-                <button>Student Signup</button>
-            </Link>
-            <Link href="/signup/eo-admin">
-                <button>EO Admin Signup</button>
-            </Link>
+            <li>
+                <Link href="/signup/school-admin">
+                    <button>School Admin Signup</button>
+                </Link>
+            </li>
+            <li>
+                <Link href="/login/students">
+                    <button>Student Signup</button>
+                </Link>
+            </li>
+            <li>
+                <Link href="/signup/eo-admin">
+                    <button>EO Admin Signup</button>
+                </Link>
+            </li>
         </div>
     );
 };
