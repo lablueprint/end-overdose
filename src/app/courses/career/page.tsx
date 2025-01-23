297c73195c3617b734637518ca00b04dc6f68e58
cheyennelu17 <cheyennelu17@g.ucla.edu>
2025-01-22 18:50:32 -0800

    making the page overflowY

--- src/app/courses/career/page.tsx
+++ src/app/courses/career/page.tsx
@@ -7,7 +7,7 @@ import { useRouter, useParams } from 'next/navigation';
 export default function CareerHome() {
     return (
         <div style={{ display: 'flex', width: '100%' }}>
-            <div style={{ flex: 3 }}>
+            <div style={{ flex: 3, overflowY: 'auto' }}>
                 <h1> Career Training </h1>
                 <div>
                     <Link href="/courses/career/lesson1">
@@ -39,8 +39,44 @@ export default function CareerHome() {
                         ></LessonTile>
                     </Link>
                 </div>
+                <div>
+                    <Link href="/courses/career/lesson1">
+                        <LessonTile
+                            lessonNumber={1}
+                            lessonTitle={'Lesson 1 Title'}
+                            lessonPath={'lesson1'}
+                            lessonCourse={'career'}
+                        ></LessonTile>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/courses/career/lesson2">
+                        <LessonTile
+                            lessonNumber={2}
+                            lessonTitle={'Lesson 2 Title'}
+                            lessonPath={'lesson2'}
+                            lessonCourse={'career'}
+                        ></LessonTile>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/courses/career/lesson3">
+                        <LessonTile
+                            lessonNumber={3}
+                            lessonTitle={'Lesson 3 Title'}
+                            lessonPath={'lesson3'}
+                            lessonCourse={'career'}
+                        ></LessonTile>
+                    </Link>
+                </div>
             </div>
-            <div style={{ flex: 7 }}>
+            <div
+                style={{
+                    flex: 7,
+                    maxHeight: '100vh',
+                    overflowY: 'auto',
+                }}
+            >
                 <SimplePage pageTitle="Career Training"></SimplePage>
             </div>
         </div>
