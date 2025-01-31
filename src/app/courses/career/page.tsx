d32ad44b77b87525be14929ec188a35013cf1dbb
cheyennelu17 <cheyennelu17@g.ucla.edu>
2025-01-30 19:56:00 -0800

    toggle finished

--- src/app/courses/career/page.tsx
+++ src/app/courses/career/page.tsx
@@ -6,17 +6,32 @@ import SimplePage from '../components/SimplePage';
 import { useRouter, useParams } from 'next/navigation';
 
 export default function CareerHome() {
-    const [toggle, setToggle] = useState(false);
+    const [toggle, setToggle] = useState(true);
     const handleClick = () => {
         setToggle((prevState) => !prevState);
     };
     return (
         <div style={{ display: 'flex', width: '100%' }}>
+            <h1
+                onClick={handleClick}
+                style={{
+                    position: 'absolute',
+                    left: '32.5rem',
+                    top: '0.5rem',
+                    fontSize: '30px',
+                    cursor: 'pointer',
+                    rotate: toggle ? '180deg' : '0deg',
+                    transform: `translateX(${toggle ? '21rem' : '0'})`,
+                }}
+            >
+                &larr;
+            </h1>
             <div
                 style={{
                     flex: toggle ? 0 : 2.8,
                     overflowY: 'auto',
                     maxHeight: '98vh',
+                    transition: 'flex-grow 0.5s ease-in-out',
                 }}
             >
                 <h1
@@ -98,17 +113,6 @@ export default function CareerHome() {
                     padding: '0 10px',
                 }}
             >
-                <h1
-                    style={{
-                        display: toggle ? 'default' : 'none',
-                        position: 'absolute',
-                        top: '10px',
-                        cursor: 'pointer',
-                    }}
-                    onClick={handleClick}
-                >
-                    →
-                </h1>
                 <SimplePage pageTitle="Career Training"></SimplePage>
             </div>
         </div>
