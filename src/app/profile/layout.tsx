7d4557dfc93f974bb583a9e2a77d4329c7ba92dc
Joyce Chen <joyce.chen721@gmail.com>
2025-03-19 02:32:36 -0700

    fix styles

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -8,7 +8,7 @@ interface AdminLayoutProps {
 const ProfileLayout: React.FC<AdminLayoutProps> = ({ children }) => {
     return (
         <AuthWrap roles={['school_admin', 'eo_admin', 'student']}>
-            <div className="courses-layout">{children}</div>
+            <>{children}</>
         </AuthWrap>
     );
 };
