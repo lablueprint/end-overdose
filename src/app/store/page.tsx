423e5fd071cfa95feca51b08780c531f653ae24c
Fiona Peng <fionapeng1616@gmail.com>
2025-05-01 21:58:14 -0700

    start store

---
+++ src/app/store/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { getKibbleFromStudentID } from '@/app/api/students/actions';
+
+export default function Score() {
+    return <div>hi</div>;
+}
