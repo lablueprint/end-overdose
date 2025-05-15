fea1b62234093eb052630676930cc271f922bc58
Fiona Peng <fionapeng1616@gmail.com>
2025-05-14 23:08:52 -0700

    delete test files

--- src/app/api/schools/testcreateschool/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { createNewSchool } from '@/app/api/schools/actions';
-
-export async function GET(req: NextRequest) {
-    const result = await createNewSchool('Fionas Superior High School');
-    return NextResponse.json(result);
-}
