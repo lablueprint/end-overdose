478c387e33fb75829d32ab599451af39c296c71c
daltonsilverman <daltonsilverman04@gmail.com>
2025-05-14 17:16:49 -0700

    Forgot to commit in last commit

---
+++ src/app/quiz/components/CurrentQuiz.tsx
@@ -0,0 +1,43 @@
+import Mcq from './Mcq';
+import TrueFalse from './TrueFalse';
+
+interface CurrentQuizProps {
+    quiz: number;
+}
+
+export default function CurrentQuiz({ quiz }: CurrentQuizProps) {
+    switch (quiz) {
+        case 0:
+            return (
+                <TrueFalse
+                    title="What are Opioids?"
+                    description="Testing what you've learned about opioids."
+                    quizIndex={0}
+                />
+            );
+        case 1:
+            return (
+                <Mcq
+                    title="What is Fentanyl?"
+                    description="Proving what you know about the drug Fentanyl."
+                    quizIndex={0}
+                />
+            );
+        case 2:
+            return (
+                <TrueFalse
+                    title="What is Opioid Overdose"
+                    description="Checking what you will do in case of an emergency."
+                    quizIndex={1}
+                />
+            );
+        case 3:
+            return (
+                <Mcq
+                    title="What is Naloxone"
+                    description="Making sure you are prepared to save lives."
+                    quizIndex={1}
+                />
+            );
+    }
+}
