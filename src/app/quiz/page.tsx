1a9db3f414f81f553cd81d824e4435c5e80fdef7
daltonsilverman <daltonsilverman04@gmail.com>
2025-05-14 17:16:31 -0700

    quiz loaded based on index

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,21 +1,12 @@
 'use client';
-import Link from 'next/link';
 import AuthWrap from '@/components/AuthWrap';
+import CurrentQuiz from './components/CurrentQuiz';
 
 export default function Quiz() {
     return (
         <AuthWrap roles={['school_admin', 'eo_admin', 'student']}>
             <div>
-                <h1> Quizzes </h1>
-                <li>
-                    <Link href="/quiz/mcq">Multiple Choice</Link>
-                </li>
-                <li>
-                    <Link href="/quiz/true-false">True/False</Link>
-                </li>
-                <li>
-                    <Link href="/game">Scenario Game</Link>
-                </li>
+                <CurrentQuiz quiz={0} />
             </div>
         </AuthWrap>
     );
