76844894db7d70b2ae72eb89249fe02f4c1432d4
Antonio Cosio <coshio@g.ucla.edu>
2025-05-17 22:02:42 -0700

    Created NavBar to match the designs on figma and other smaller changes to align with designs

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -66,7 +66,7 @@ export default function SignUpPage() {
             setError('Need to Agree to Terms of Serivice');
             return
         }
-        
+
         //2. The rest of the boxed need to be filled 
         if (!email || !password || !role || !school) {
             setError('Need to fill out all the fields')
@@ -86,9 +86,9 @@ export default function SignUpPage() {
             if (role == "admin") { //school admin
                 // 2) Build your Admin object
                 const newSchoolAdmin: NewSchoolAdmin = {
-                approved: false,
-                email: email,
-                school_id: "",
+                    approved: false,
+                    email: email,
+                    school_id: "",
                 }
 
                 // 3) Write it into your “newSchoolAdmins” collection
@@ -112,7 +112,7 @@ export default function SignUpPage() {
             setTimeout(() => {
                 router.push('/signin');
             }, 1000);
-            
+
         } catch (err) {
             console.error(err);
             setError('Something went wrong.');
@@ -125,170 +125,172 @@ export default function SignUpPage() {
     }, []);
 
     return (
-        <div className={styles.splitContainer}>
-            <div className={styles.loginHalf}>
-                <div className={styles.contentContainer}>
-                    <div className={styles.bodyContainer}>
-                        <div className={styles.titleTextContainer}>
-                            <h1 className={styles.h1}>CREATE AN ACCOUNT</h1>
-                            <h2 className={styles.h2}>
-                                We&apos;re so glad you could join us!
-                            </h2>
-                        </div>
-                        {error && <p style={{ color: 'red' }}>{error}</p>}
-                        {success && (
-                            <p style={{ color: 'green' }}>Signup successful!</p>
-                        )}
-                        <div className={styles.formContainer}>
-                            <form
-                                className={styles.form}
-                                onSubmit={handleSubmit(onSubmit)}
-                            >
-                                <div className={styles.subForm}>
-                                    <label className={styles.h2} htmlFor="role">
-                                        Select your Role
-                                    </label>
-                                    <select
-                                        className={`${styles.input} ${styles.formControl}`}
-                                        id="role"
-                                        {...register('role', {
-                                            required: true,
-                                        })}
-                                    >
-                                        <option value="">Choose a Role</option>
-                                        <option value="eo_admin">EO Admin</option>
-                                        <option value="admin">School Admin</option>
-                                    </select>
-                                </div>
+        <div className='bg-[#0C1321]'>
+            <div className={styles.splitContainer}>
+                <div className={styles.loginHalf}>
+                    <div className={styles.contentContainer}>
+                        <div className={styles.bodyContainer}>
+                            <div className={styles.titleTextContainer}>
+                                <h1 className={styles.h1}>CREATE AN ACCOUNT</h1>
+                                <h2 className={styles.h2}>
+                                    We&apos;re so glad you could join us!
+                                </h2>
+                            </div>
+                            {error && <p style={{ color: 'red' }}>{error}</p>}
+                            {success && (
+                                <p style={{ color: 'green' }}>Signup successful!</p>
+                            )}
+                            <div className={styles.formContainer}>
+                                <form
+                                    className={styles.form}
+                                    onSubmit={handleSubmit(onSubmit)}
+                                >
+                                    <div className={styles.subForm}>
+                                        <label className={styles.h2} htmlFor="role">
+                                            Select your Role
+                                        </label>
+                                        <select
+                                            className={`${styles.input} ${styles.formControl}`}
+                                            id="role"
+                                            {...register('role', {
+                                                required: true,
+                                            })}
+                                        >
+                                            <option value="">Choose a Role</option>
+                                            <option value="eo_admin">EO Admin</option>
+                                            <option value="admin">School Admin</option>
+                                        </select>
+                                    </div>
 
-                                <div className={styles.subForm}>
-                                    <label
-                                        className={styles.h2}
-                                        htmlFor="school"
-                                    >
-                                        Select your School
-                                    </label>
-                                    <select
-                                        className={`${styles.input} ${styles.formControl}`}
-                                        id="school"
-                                        {...register('school', { required: true })}
-                                    >
-                                        <option value="">
-                                            Choose a School
-                                        </option>
-                                        {schoolValues}
-                                    </select>
-                                </div>
+                                    <div className={styles.subForm}>
+                                        <label
+                                            className={styles.h2}
+                                            htmlFor="school"
+                                        >
+                                            Select your School
+                                        </label>
+                                        <select
+                                            className={`${styles.input} ${styles.formControl}`}
+                                            id="school"
+                                            {...register('school', { required: true })}
+                                        >
+                                            <option value="">
+                                                Choose a School
+                                            </option>
+                                            {schoolValues}
+                                        </select>
+                                    </div>
 
-                                <div className={styles.subForm}>
-                                    <label
-                                        className={styles.h2}
-                                        htmlFor="email"
-                                    >
-                                        Email address:
-                                    </label>
-                                    <input
-                                        className={`${styles.input} ${styles.formControl}`}
-                                        type="email"
-                                        id="email"
-                                        {...register('email', {
-                                            required: true,
-                                        })}
-                                    />
-                                </div>
-
-                                <div className={styles.subForm}>
-                                    <label
-                                        className={styles.h2}
-                                        htmlFor="password"
-                                    >
-                                        Password:
-                                    </label>
-                                    <input
-                                        className={`${styles.input} ${styles.formControl}`}
-                                        type="password"
-                                        id="password"
-                                        {...register('password', {
-                                            required: true,
-                                        })}
-                                    />
-                                </div>
-
-                                <div className={styles.checkboxContainer}>
-                                    <div className={styles.checkboxGroup}>
+                                    <div className={styles.subForm}>
+                                        <label
+                                            className={styles.h2}
+                                            htmlFor="email"
+                                        >
+                                            Email address:
+                                        </label>
                                         <input
-                                            type="checkbox"
-                                            id="termsAgreed"
-                                            className={styles.checkbox}
-                                            {...register('termsAgreed', {
+                                            className={`${styles.input} ${styles.formControl}`}
+                                            type="email"
+                                            id="email"
+                                            {...register('email', {
                                                 required: true,
                                             })}
                                         />
-                                        <label
-                                            htmlFor="termsAgreed"
-                                            className={styles.checkboxLabel}
-                                        >
-                                            Agree to our{' '}
-                                            <Link
-                                                href="/terms"
-                                                className={styles.link}
-                                            >
-                                                Terms of use
-                                            </Link>{' '}
-                                            and{' '}
-                                            <Link
-                                                href="/privacy"
-                                                className={styles.link}
-                                            >
-                                                Privacy Policy
-                                            </Link>
-                                        </label>
                                     </div>
 
-                                    <div className={styles.checkboxGroup}>
+                                    <div className={styles.subForm}>
+                                        <label
+                                            className={styles.h2}
+                                            htmlFor="password"
+                                        >
+                                            Password:
+                                        </label>
                                         <input
-                                            type="checkbox"
-                                            id="newsletter"
-                                            className={styles.checkbox}
-                                            {...register('newsletter', {
+                                            className={`${styles.input} ${styles.formControl}`}
+                                            type="password"
+                                            id="password"
+                                            {...register('password', {
                                                 required: true,
                                             })}
                                         />
-                                        <label
-                                            htmlFor="newsletter"
-                                            className={styles.checkboxLabel}
-                                        >
-                                            Subscribe to our monthly newsletter
-                                        </label>
                                     </div>
-                                </div>
 
-                                <div className={styles.buttonContainer}>
-                                    <button
-                                        className={styles.loginButton}
-                                        type="submit"
-                                    >
-                                        SIGN UP
-                                    </button>
-                                    <div className={styles.navigationContainer}>
-                                        <h2 className={styles.h2}>
-                                            {`Already have an account?   `}
-                                            <Link
-                                                className={styles.link}
-                                                href="/signin"
+                                    <div className={styles.checkboxContainer}>
+                                        <div className={styles.checkboxGroup}>
+                                            <input
+                                                type="checkbox"
+                                                id="termsAgreed"
+                                                className={styles.checkbox}
+                                                {...register('termsAgreed', {
+                                                    required: true,
+                                                })}
+                                            />
+                                            <label
+                                                htmlFor="termsAgreed"
+                                                className={styles.checkboxLabel}
                                             >
-                                                SIGN IN
-                                            </Link>
-                                        </h2>
+                                                Agree to our{' '}
+                                                <Link
+                                                    href="/terms"
+                                                    className={styles.link}
+                                                >
+                                                    Terms of use
+                                                </Link>{' '}
+                                                and{' '}
+                                                <Link
+                                                    href="/privacy"
+                                                    className={styles.link}
+                                                >
+                                                    Privacy Policy
+                                                </Link>
+                                            </label>
+                                        </div>
+
+                                        <div className={styles.checkboxGroup}>
+                                            <input
+                                                type="checkbox"
+                                                id="newsletter"
+                                                className={styles.checkbox}
+                                                {...register('newsletter', {
+                                                    required: true,
+                                                })}
+                                            />
+                                            <label
+                                                htmlFor="newsletter"
+                                                className={styles.checkboxLabel}
+                                            >
+                                                Subscribe to our monthly newsletter
+                                            </label>
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
+
+                                    <div className={styles.buttonContainer}>
+                                        <button
+                                            className={styles.loginButton}
+                                            type="submit"
+                                        >
+                                            SIGN UP
+                                        </button>
+                                        <div className={styles.navigationContainer}>
+                                            <h2 className={styles.h2}>
+                                                {`Already have an account?   `}
+                                                <Link
+                                                    className={styles.link}
+                                                    href="/signin"
+                                                >
+                                                    SIGN IN
+                                                </Link>
+                                            </h2>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className={styles.placeHolderHalf}>
-                <div className={styles.narcat}></div>
+                <div className={styles.placeHolderHalf}>
+                    <div className={styles.narcat}></div>
+                </div>
             </div>
         </div>
     );
